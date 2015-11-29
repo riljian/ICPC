5d5301a90ea80544ef50ce7ec770f1ce550229dc
Ril Jian <rilislearning@gmail.com>
2015-11-29 23:14:13 +0800

    Problem about dominator, bridge

---
+++ accept/uva10765.cpp
@@ -0,0 +1,105 @@
+/* cut-edge, bridge */
+#include <cstdio>
+#include <algorithm>
+#include <cstring>
+using namespace std;
+#define MAXN 10005
+
+struct Edge {
+	int to;
+	int next;
+	bool usable;
+} edge[MAXN * 10];
+
+int graph[MAXN];
+int cnt_edge;
+int low[MAXN];
+int dfn[MAXN];
+int pigeon[MAXN];
+int cnt_dfn;
+int ans[MAXN];
+
+void init_graph() {
+	memset(graph, -1, sizeof(graph));
+	cnt_edge = 0;
+}
+
+void push_edge(int s, int t) {
+	edge[cnt_edge].to = t;
+	edge[cnt_edge].next = graph[s];
+	edge[cnt_edge].usable = true;
+	graph[s] = cnt_edge;
+	++cnt_edge;
+}
+
+void bridge(int p, int n) {
+	low[n] = dfn[n] = ++cnt_dfn;
+	for (int e = graph[n]; ~e; e = edge[e].next) {
+		if (!dfn[edge[e].to]) {
+			bridge(n, edge[e].to);
+			low[n] = min(low[n], low[edge[e].to]);
+			if (low[edge[e].to] > dfn[n]) {
+				edge[e].usable = false;
+				edge[e ^ 1].usable = false;
+				++pigeon[n];
+				++pigeon[edge[e].to];
+			}
+		} else if (edge[e].to != p) {
+			low[n] = min(low[n], low[edge[e].to]);
+		}
+	}
+}
+
+void dfs(int s) {
+	for (int e = graph[s]; ~e; e = edge[e].next) {
+		if (edge[e].usable && !dfn[edge[e].to]) {
+			dfn[edge[e].to] = 1;
+			dfs(edge[e].to);
+		}
+	}
+}
+
+void init() {
+	init_graph();
+	cnt_dfn = 0;
+	memset(dfn, 0, sizeof(dfn));
+	memset(low, 0, sizeof(low));
+	memset(pigeon, 0, sizeof(pigeon));
+}
+
+bool compare(int i, int j) {
+	if (pigeon[i] != pigeon[j]) {
+		return pigeon[i] > pigeon[j];
+	} else {
+		return i < j;
+	}
+}
+
+int main() {
+	int n, m, s, t;
+	while (scanf("%d %d", &n, &m) && (m + n)) {
+		init();
+		while (scanf("%d %d", &s, &t) && s >= 0) {
+			push_edge(s, t);
+			push_edge(t, s);
+		}
+		bridge(-1, 0);
+		memset(dfn, 0, sizeof(dfn));
+		for (int i = 0; i < n; ++i) {
+			if (!dfn[i]) {
+				dfs(i);
+			}
+			pigeon[i] += dfn[i];
+			ans[i] = i;
+		}
+		sort(ans, ans + n, compare);
+		for (int i = 0; i < m; ++i) {
+			if (i) {
+				putchar('\n');
+			}
+			printf("%d %d", ans[i], pigeon[ans[i]]);
+		}
+		puts("\n");
+	}
+	return 0;
+}
---
+++ accept/uva11902.cpp
@@ -0,0 +1,91 @@
+/* depth-first-search, dominator */
+#include <cstdio>
+#include <set>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+#define MAXN 105
+
+struct Edge {
+	int to;
+	int next;
+} edge[MAXN * MAXN];
+
+int graph[MAXN];
+int cnt_edge;
+bool ans[MAXN];
+bool visited[MAXN];
+
+void init_graph() {
+	memset(graph, -1, sizeof(graph));
+	cnt_edge = 0;
+}
+
+void push_edge(int s, int t) {
+	edge[cnt_edge].to = t;
+	edge[cnt_edge].next = graph[s];
+	graph[s] = cnt_edge;
+	++cnt_edge;
+}
+
+void dfs(int s, int x) {
+	if (s != x) {
+		visited[s] = true;
+		for (int e = graph[s]; ~e; e = edge[e].next) {
+			if (!visited[edge[e].to]) {
+				dfs(edge[e].to, x);
+			}
+		}
+	}
+
+}
+
+int main() {
+	int n, t, cases;
+	char hr[MAXN * 2], output[MAXN * 2];
+	bool original[MAXN];
+	scanf("%d", &cases);
+	for (int c = 1; c <= cases; ++c) {
+		scanf("%d", &n);
+		init_graph();
+		for (int i = 0; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				scanf("%d", &t);
+				if (t) {
+					push_edge(i, j);
+				}
+			}
+		}
+		printf("Case %d:\n", c);
+		for (int i = 0; i < 2 * n + 1; ++i) {
+			if (i == 0 || i == 2 * n) {
+				hr[i] = '+';
+			} else {
+				hr[i] = '-';
+			}
+		}
+		hr[2 * n + 1] = '\0';
+		memset(visited, false, sizeof(visited));
+		dfs(0, -1);
+		memcpy(original, visited, n * sizeof(bool));
+		for (int i = 0; i < n; ++i) {
+			memset(visited, false, sizeof(visited));
+			dfs(0, i);
+			for (int j = 0; j < n; ++j) {
+				ans[j] = original[j] ^ visited[j];
+			}
+			if (!i) {
+				puts(hr);
+			}
+			for (int j = 0; j < n; ++j) {
+				output[2 * j] = '|';
+				output[2 * j + 1] = ans[j] ? 'Y' : 'N';
+			}
+			output[2 * n] = '|';
+			output[2 * n + 1] = '\0';
+			puts(output);
+			puts(hr);
+		}
+	}
+	return 0;
+}
