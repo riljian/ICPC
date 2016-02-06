1dc570bc14d2b971119026ddc66ed03ca5d7f44a
Ril Jian <rilislearning@gmail.com>
2016-02-06 16:06:34 +0800

    UVa 11984

---
+++ accept/uva11984.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int n, c = 0;
+    float a, b;
+    scanf("%d", &n);
+    while (n--) {
+        scanf("%f%f", &a, &b);
+        printf("Case %d: %.2f\n", ++c, a + b * 5 / 9);
+    }
+    return 0;
+}
