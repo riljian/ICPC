c9c4a5fefb66a3a12a76f1a7c7945168487438b2
Ril Jian <rilislearning@gmail.com>
2016-03-05 12:41:58 +0800

    POJ 1450

--- accept/poj1450.cpp
+++ accept/poj1450.cpp
@@ -3,13 +3,18 @@ using namespace std;
 
 int main() {
     int a, b, c = 0, cases;
+    float ans;
     scanf("%d", &cases);
     while (c < cases) {
         if (c) {
             putchar('\n');
         }
         scanf("%d%d", &a, &b);
-        printf("Scenario #%d\n%d.00\n", ++c, a * b);
+        ans = (float) a * b;
+        if (a & b & 1) {
+            ans += 0.41;
+        }
+        printf("Scenario #%d:\n%.2f\n", ++c, ans);
     }
     return 0;
 }
